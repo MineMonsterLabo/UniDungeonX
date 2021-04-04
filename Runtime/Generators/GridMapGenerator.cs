ff7c3124d47c70ccd80c3ebc473a8eda1974934a
relipmoc101 <relipmoc101@gmail.com>
2021-04-04 18:53:18 +0900

    道も作成できるように

--- Runtime/Generators/GridMapGenerator.cs
+++ Runtime/Generators/GridMapGenerator.cs
@@ -11,6 +11,9 @@ namespace UniDungeonX.Generators
             foreach (var room in structure.rooms)
                 PutRoom(cellKinds, room);
 
+            foreach (var road in structure.roads)
+                PutRoad(cellKinds, road);
+
             return new GridMap(cellKinds);
         }
 
@@ -41,9 +44,43 @@ namespace UniDungeonX.Generators
             }
         }
 
+        public static void PutRoad(CellKind[,] cellKinds, Road road)
+        {
+            if (road.isVertical)
+            {
+                for (var y = 0; y < road.length; y++)
+                {
+                    PutCell(cellKinds, 0, y, road.leftDownPos, CellKind.Ground);
+                    PutWeakCell(cellKinds, 1, y, road.leftDownPos, CellKind.RightEdgeWall);
+                    PutWeakCell(cellKinds, -1, y, road.leftDownPos, CellKind.LeftEdgeWall);
+                }
+                PutWeakCell(cellKinds, 0, -1, road.leftDownPos, CellKind.DownEdgeWall);
+                PutWeakCell(cellKinds, 0, road.length, road.leftDownPos, CellKind.UpEdgeWall);
+            }
+            else
+            {
+                for (var x = 0; x < road.length; x++)
+                {
+                    PutCell(cellKinds, x, 0, road.leftDownPos, CellKind.Ground);
+                    PutWeakCell(cellKinds, x, 1, road.leftDownPos, CellKind.UpEdgeWall);
+                    PutWeakCell(cellKinds, x, -1, road.leftDownPos, CellKind.DownEdgeWall);
+                }
+                PutWeakCell(cellKinds, -1, 0, road.leftDownPos, CellKind.LeftEdgeWall);
+                PutWeakCell(cellKinds, road.length, 0, road.leftDownPos, CellKind.RightEdgeWall);
+            }
+        }
+
         private static void PutCell(CellKind[,] cellKinds, int x, int y, Position leftDownPos, CellKind cellKind)
         {
             cellKinds[x + 1 + leftDownPos.x, y + 1 + leftDownPos.y] = cellKind;
+        }
+
+        private static void PutWeakCell(CellKind[,] cellKinds, int x, int y, Position leftDownPos, CellKind cellKind)
+        {
+            if (cellKinds[x + 1 + leftDownPos.x, y + 1 + leftDownPos.y] == CellKind.Empty)
+            {
+                PutCell(cellKinds, x, y, leftDownPos, cellKind);
+            };
         }
     }
 }
